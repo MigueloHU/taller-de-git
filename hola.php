9754884e7e8194bb757b029154f15eb124cc7b3b
Miguel Calero <miguelcalero_84@hotmail.com>
2025-05-27 04:34:35 +0200

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,3 +1,6 @@
 #Miguel Calero Contreras
 <?php
-@print "Hola {$argv[1]}\n";
+// Autor: Sergio Gómez <sergio@uco.es>
+// El nombre por defecto es Mundo
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";
